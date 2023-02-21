72bfb0e945834445174a7443f5e3e644f8ea0177
BIGOSCAR85 <rojas.osc85@gmail.com>
2023-02-21 19:40:46 +0000

    mi primer commit asegurando codigo y funcionamiento

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container">
+				<Link to="/">
+					<img
+						height="75px"
+						src="https://imagenesparapeques.com/wp-content/uploads/2016/01/Star-Wars-Logo.png" width="200"
+						className="navbar-brand mb-0 h1"
+					/>
 				</Link>
+				<div className="dropdown">
+					<button className="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites
+					</button>
+					<ul className="dropdown-menu">
+						{store.favorites.map((favorite, index) => {
+							return <li key={index}>
+								<a
+									className="dropdown-item d-flex justify-content-between align-items-center"
+									href="#">
+									{favorite.name}
+									<i className="btn btn-danger btn-sm fas fa-trash ms-3" onClick={(event) => actions.deleteFavorite(favorite)}></i>
+								</a>
+							</li>
+						})}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,73 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useState, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Card } from "../component/Card.jsx";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  let count_characters = 0;
+  let count_planets = 0;
+  let img = "";
+
+  useEffect(() => {
+    actions.getCharacters();
+    actions.getPlanets();
+  }, []);
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="title">Characters</h1>
+      </div>
+      <div className="d-flex flex-row char-container">
+        {store.characters.length <= 0 ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {" "}
+            {store.characters.map((character, index) => {
+              count_characters = count_characters + 1;
+              let name_characters = "people";
+              img = `https://starwars-visualguide.com/assets/img/characters/${count_characters}.jpg`;
+              return (
+                <Card
+                  item={character}
+                  key={index}
+                  picture={img}
+                  id={count_characters}
+                  type={name_characters}
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
+
+      <div>
+        <h1 className="title">Planets</h1>
+      </div>
+      <div className="d-flex flex-row char-container">
+        {store.planets.length <= 0 ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {store.planets.map((planet, index) => {
+              count_planets = count_planets + 1;
+              let name_planets = "planets";
+              img = `https://starwars-visualguide.com/assets/img/planets/${count_planets}.jpg`;
+              return (
+                <Card
+                  item={planet}
+                  key={`${index}a`}
+                  picture={img}
+                  id={count_planets}
+                  type={name_planets}
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
